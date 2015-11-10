cebc536510fcf2901e4b5f67771bbc0cf252b956
Kevin Y Liu <kevin.y.liu@vanderbilt.edu>
2015-11-10 12:02:03 -0600

    added powerups

---
+++ FinalProject/Assets/PowerUps/PowerUp2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp2 : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with PowerUp 2");
+        collision.gameObject.GetComponent<RocketShoot>().BulletCD = 0.05f;
+        Destroy(this.gameObject);
+    }
+}
