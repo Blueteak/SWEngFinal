d0f45eec46b9b78c4a42f3e547a2e72baaa80566
unknown <jonathan.a.clinton@vanderbilt.edu>
2015-11-12 11:57:44 -0600

    Fixed cursor

---
+++ FinalProject/Assets/GameScripts/Setup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Setup : MonoBehaviour {
+    public Texture2D cursor;
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(cursor, Vector2.one/2, CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
