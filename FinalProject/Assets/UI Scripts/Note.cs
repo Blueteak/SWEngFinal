286b2003680c86157589cab7564ce3b6d5b32b99
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2015-11-14 23:12:31 -0600

    Main Menu Setup
    • Popup Message Framework
    • Main Menu v0.1
    • Login System v0.2

---
+++ FinalProject/Assets/UI Scripts/Note.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Note : MonoBehaviour {
+
+	EMOpenCloseMotion motion;
+	public Text TitleText;
+	public Text MessageText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		motion = GetComponent<EMOpenCloseMotion>();
+	}
+	
+	public void Notify(string title, string message, float displayTime)
+	{
+		TitleText.text = title;
+		MessageText.text = message;
+		DoNoteify(displayTime);
+	}
+
+	public void Warin(string message)
+	{
+		Warn(message, 3f);
+	}
+
+	public void Warn(string message, float displayTime)
+	{
+		TitleText.text = "Warning!";
+		MessageText.text = message;
+		DoNoteify(displayTime);
+	}
+
+	public void Error(string message)
+	{
+		Error(message, 3f);
+	}
+
+	public void Error(string message, float displayTime)
+	{
+		TitleText.text = "Error";
+		MessageText.text = message;
+		DoNoteify(displayTime);
+	}
+
+	void DoNoteify(float time)
+	{
+		StopAllCoroutines();
+		StartCoroutine("ShowAndHide", time);
+	}
+
+	IEnumerator ShowAndHide(float seconds)
+	{
+		motion.SetStateToClose();
+		motion.Open();
+		yield return new WaitForSeconds(seconds);
+		motion.SetStateToClose();
+		motion.Close();
+	}
+}
---
+++ FinalProject/Assets/UI Scripts/UserLogin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserLogin : MonoBehaviour 
+{
+	
+	public EMOpenCloseMotion ThisPanel;
+	public EMOpenCloseMotion MenuPanel;
+
+	public void SetUsername(string username)
+	{
+		if(PhotonNetwork.insideLobby)
+		{
+			Debug.Log("Logged in as: " + username);
+			PhotonNetwork.playerName = username;
+			ThisPanel.Close();
+			MenuPanel.Open();
+		}
+		else
+		{
+			FindObjectOfType<Note>().Error("Not yet connected.");
+		}
+	}
+}
