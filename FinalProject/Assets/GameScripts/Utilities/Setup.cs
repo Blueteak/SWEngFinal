5be2ad2f7ead2f149fc9efceaeb3212e93e684eb
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2015-11-17 11:03:12 -0600

    Added Menu Interaction
    • Grid interaction within menus

--- FinalProject/Assets/GameScripts/Utilities/Setup.cs
+++ FinalProject/Assets/GameScripts/Utilities/Setup.cs
@@ -5,7 +5,7 @@ public class Setup : MonoBehaviour {
     public Texture2D cursor;
 	// Use this for initialization
 	void Start () {
-        Cursor.SetCursor(cursor, Vector2.one/2, CursorMode.Auto);
+        Cursor.SetCursor(cursor, new Vector2(cursor.width/2, cursor.height/2) , CursorMode.ForceSoftware);
 	}
 	
 	// Update is called once per frame
---
+++ FinalProject/Assets/VectorGrid/Scripts/GridMouse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMouse : MonoBehaviour {
+
+	public bool inMenu = true;
+	RocketControl rocket;
+	VectorGrid grid;
+
+	void Start()
+	{
+		grid = FindObjectOfType<VectorGrid>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckInMenu();
+		if(Input.GetMouseButton(0))
+		{
+			Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			mPos.z = 0;
+			grid.AddGridForce(mPos, .5f, .8f, Color.white, false);
+		}
+	}
+
+	void CheckInMenu()
+	{
+		if(rocket == null)
+			rocket = FindObjectOfType<RocketControl>();
+
+		if(rocket == null)
+			inMenu = true;
+		else
+			inMenu = false;
+	}
+}
