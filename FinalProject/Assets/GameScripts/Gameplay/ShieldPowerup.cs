df1bd9b7c48cfd7dbf0bf25e3ef6bf376ecacbc4
Frank Cao <r33dundent@gmail.com>
2015-11-18 22:57:52 -0600

    added gamescripts for powerups for health and shield, unsure how to do network stuff

---
+++ FinalProject/Assets/GameScripts/Gameplay/ShieldPowerup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldPowerup : MonoBehaviour {
+    bool pickedUp = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (pickedUp == false)
+        {
+            Debug.Log("Shield Picked Up");
+            collision.gameObject.GetComponent<RocketHealth>().curShield = 200;
+            this.GetComponent<Renderer>().enabled = false;
+            pickedUp = true;
+        }
+        Invoke("RespawnPowerup", 5.0f);
+
+    }
+
+    void RespawnPowerup()
+    {
+        this.GetComponent<Renderer>().enabled = true;
+        pickedUp = false;
+    }
+}
