3247d87ae0204ddfaba43436728a409667cae1c0
Kevin Y Liu <kevin.y.liu@vanderbilt.edu>
2015-11-19 11:23:39 -0600

    bulletcd, shield

---
+++ FinalProject/Assets/bulletSpeedPowerup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletSpeedPowerup : MonoBehaviour
+{
+
+    public int buffTimer = 15;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with Powerup");
+        collision.gameObject.GetComponent<RocketShoot>().BulletCD = 0.1f;
+        Destroy(this.gameObject);
+    }
+
+    
+}
