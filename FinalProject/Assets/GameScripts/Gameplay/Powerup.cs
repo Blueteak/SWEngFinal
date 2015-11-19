ed0a95878af79e2bc0889e76b57da1752c4be52e
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2015-11-19 10:11:34 -0600

    Added Powerup Animation
    • Spawn/Despawn

--- FinalProject/Assets/GameScripts/Gameplay/Powerup.cs
+++ FinalProject/Assets/GameScripts/Gameplay/Powerup.cs
@@ -5,7 +5,15 @@ using System.Collections;
 public class Powerup : MonoBehaviour {
 
 	public float RespawnTime;
+	private float StartScale;
     bool pickedUp = false;
+
+	void Start()
+	{
+		StartScale = transform.localScale.x;
+		transform.localScale = Vector3.zero;
+		StartCoroutine("Spawn");
+	}
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,13 +28,35 @@ public class Powerup : MonoBehaviour {
 	[PunRPC]
 	public virtual void PickupObj()
 	{
-		GetComponent<Renderer>().enabled = false;
 		pickedUp = true;
+		StopCoroutine("Spawn");
+		StartCoroutine("Despawn");
+	}
+
+	IEnumerator Despawn()
+	{
+		while(transform.localScale.x > 0.01f)
+		{
+			transform.localScale -= Time.deltaTime*Vector3.one*4*StartScale;
+			yield return true;
+		}
+		transform.localScale = Vector3.zero;	
+	}
+
+	IEnumerator Spawn()
+	{
+		while(transform.localScale.x < StartScale)
+		{
+			transform.localScale += Time.deltaTime*Vector3.one*2*StartScale;
+			yield return true;
+		}
+		transform.localScale = Vector3.one*StartScale;
 	}
 
     void RespawnPowerup()
     {
-        this.GetComponent<Renderer>().enabled = true;
+		StopCoroutine("Despawn");
+		StartCoroutine("Spawn");
         pickedUp = false;
     }
 
