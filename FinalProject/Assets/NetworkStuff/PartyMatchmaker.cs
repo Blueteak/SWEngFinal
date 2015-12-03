22378432d13c8cb60f5201be255bb33643547a7e
unknown <jonathan.a.clinton@vanderbilt.edu>
2015-12-03 11:59:18 -0600

    added initial party system script

---
+++ FinalProject/Assets/NetworkStuff/PartyMatchmaker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartyMatchmaker : MonoBehaviour {
+
+    private List<string> partyList = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void AddPlayerToParty(string player)
+    {
+        partyList.Add(player);
+    }
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(PhotonNetwork.playerName, new RoomOptions() { maxPlayers = 5 }, null);
+        //need to ask others to join room
+    }
+}
